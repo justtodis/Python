e244728988c8c140adf9c9918ff74c377b70c047
justtodis <flyingswag2@gmail.com>
2024-04-09 11:59:32 +0200

    Update 07-while.py

--- control-flujo/07-while.py
+++ control-flujo/07-while.py
@@ -8,3 +8,9 @@ comando = ""
 while comando.lower != "salir": #mientras comando sea diferente a salir , lower hace que lo que escribas se haga minusc
     comando = input("$ ") # comando es igual a input (input del usuario)
     print(comando)
+
+while True:  # esto sera un loop infinito y solo se parara usando salir como vemos abajo
+    comando = input("$ ") 
+    print(comando)
+    if comando.lower() == "salir": # aqui todo lo que escribas lo leera como salir aunque pongas SALIR o SaLiR
+        break
